9e3fd97a0ad894d54251c8e28307ca522b780fb4
Marta Probierz <75137091+marta-probierz@users.noreply.github.com>
2022-06-04 12:59:08 +0200

    features: add mail (#5)

---
+++ server/src/models/userModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+const userSchema = new mongoose.Schema({});
+
+export const validateUser = (user) => {
+	const schema = Joi.object({
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			maxlength: 250,
+			minlength: 5,
+		},
+		// artykuły
+	});
+
+	return schema.validate(user);
+};
+
+export const Question = mongoose.model("User", userSchema);
