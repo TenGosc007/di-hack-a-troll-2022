75b9f32ecfc1976c7ab4a4dc8350c66da2794595
Aleksander Atamańczuk <aleksander.atamanczuk@gmail.com>
2022-06-04 13:36:23 +0200

    add default route

--- server/src/app/routes.js
+++ server/src/app/routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import error from '../middleware/error.js';
+import defautlRoute from '../routes/default.js';
 import questions from '../routes/questions.js';
 import categories from '../routes/categories.js';
 
 export const routes = (app) => {
   app.use(express.json());
+  app.use('/', defautlRoute);
   app.use('/api/questions', questions);
   app.use('/api/categories', categories);
   app.use(error);
