484bd97668fdbef8d12e556386d769f3e8f94d47
Aleksander Atamańczuk <aleksander.atamanczuk@gmail.com>
2022-06-05 07:34:59 +0200

    refactor

--- client/src/pages/SurveyInfoPage/SurveyInfoPage.jsx
+++ client/src/pages/SurveyInfoPage/SurveyInfoPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
