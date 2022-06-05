f501ea2fd1ffc1952d375694a114c105d16fa7e4
Aleksander Atamańczuk <aleksander.atamanczuk@gmail.com>
2022-06-05 05:28:07 +0200

    add site stats

--- server/src/routes/scores.js
+++ server/src/routes/scores.js
@@ -1,8 +1,10 @@
 import express from 'express';
 
 import { articleScores } from '../controllers/scores/articleScores.js';
+import { siteScores } from '../controllers/scores/siteScores.js';
 
 const router = express.Router();
-router.get('/:id', articleScores);
+router.get('/articles/:id', articleScores);
+router.get('/sites/:id', siteScores);
 
 export default router;
