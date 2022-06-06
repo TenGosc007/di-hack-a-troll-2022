35904caf247a0a25795508728e4c95ff0cd9be94
Aleksander <aleksander.atamanczuk@gmail.com>
2022-06-06 22:03:23 +0200

    udpdate get articles

--- server/src/controllers/articles/getArticles.js
+++ server/src/controllers/articles/getArticles.js
@@ -1,6 +1,23 @@
+import _ from 'lodash';
+
 import { Article } from '../../models/articleModel.js';
+import { Category } from '../../models/categoryModel.js';
 
 export const getArticles = async (req, res) => {
   const articles = await Article.find();
-  res.send(articles);
+  const categories = await Category.find();
+
+  const result = articles.map((article) => ({
+    ...article._doc,
+    categories: article.categories
+      .map((catId) =>
+        _.pick(
+          categories.find(({ _id }) => _id.toString() === catId),
+          ['_id', 'name']
+        )
+      )
+      .filter((e) => !!e.name),
+  }));
+
+  res.send(result);
 };
